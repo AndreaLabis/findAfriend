e50dffa6d51ef3baa6c7273f7276c4b4138af1d0
Andrea Labis <sweetlila111282@aol.com>
2020-03-19 23:43:46 -0400

    committing updated js files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,8 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of our friend information.
-// ===============================================================================
 
-var friends = [
+
+var friendsData = [
     {
-        "name": "Ahmed",
+        "name": "Alan Chevy",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
         "scores": [
         "5",
@@ -22,7 +19,7 @@ var friends = [
         },
 
         {
-        "name": "Jacob Deming",
+        "name": "Jacob Macon",
         "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
         "scores": [
         "4",
@@ -39,7 +36,7 @@ var friends = [
         },
 
         {
-        "name": "Jeremiah Scanlon",
+        "name": "Jerry Seinfeld",
         "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
         "scores": [
         "5",
@@ -56,7 +53,7 @@ var friends = [
         },
 
         {
-        "name": "Louis T. Delia",
+        "name": "Donald Duck",
         "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
         "scores": [
         "3",
@@ -73,7 +70,7 @@ var friends = [
         },
 
         {
-        "name": "Lou Ritter",
+        "name": "Louis Rinehorn",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
         "scores": [
         "4",
@@ -90,7 +87,7 @@ var friends = [
         },
 
         {
-        "name": "Jordan Biason",
+        "name": "John Wick",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
         "scores": [
         "4",
@@ -107,7 +104,7 @@ var friends = [
         },
 
         {
-        "name": "Murad Cholukov",
+        "name": "Joe Shmoe",
         "photo": "www.mypic.com",
         "scores": [
         "2",
@@ -139,103 +136,6 @@ var friends = [
         "1"
         ]
         }
-    ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   ];
-  
-  // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = friends;
+ 
+  module.exports = friendsData;
